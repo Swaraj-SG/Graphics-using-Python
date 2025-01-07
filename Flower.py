cc8abdce8584dbb63647bd4d19f226d397a467cd
Swaraj Gurav <swarajsgurav@gmail.com>
2025-01-07 19:51:18 +0530

    Flower.py

--- Flower.py
+++ Flower.py
@@ -10,7 +10,7 @@ for i in range(16):
     for j in range(18):
         c=colorsys.hsv_to_rgb(h, 1, 1)
         color(c)
-        h+=0.005
+        h+=0.1
         rt(90)
         circle(150-j*6, 90)
         lt(90)
